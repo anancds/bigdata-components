406d7b5c6a7e8c8782a63656c9f6ad53f14e9405
anancds <anancds@163.com>
2017-03-14 14:45:22 +0800

    Added regular expression

---
+++ regular-expression-learn/src/main/java/com/cds/learn/RegularExpression.java
@@ -0,0 +1,42 @@
+package com.cds.learn;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularExpression {
+
+  private static void test1() {
+    String options = "\"options\":\"zk '10.6.131.144',hbaseTableName 'test',indexerClass 'com.hikvision.bigdata.ude.datamanipulator.EsIndexer',indexerURL '10.6.131.144:9300:allsomtype/allsomtype'\"";
+    System.out.println(options);
+    Pattern tableP = Pattern.compile("hbaseTableName\\s+'([^,']+)'");
+    Matcher tableM = tableP.matcher(options);
+    System.out.println("hbaseTabelName: " + tableM.groupCount());
+    if (tableM.find() && tableM.groupCount() == 1) {
+      System.out.println("hbaseTabelName: " + tableM.group(1));
+    }
+
+
+    Pattern zkP = Pattern.compile("zk\\s+'([^']+)'");
+    Matcher zkM = zkP.matcher(options);
+    if (zkM.find()) {
+      System.out.println("zk: " + zkM.group(1));
+    }
+  }
+
+  private static void test2() {
+
+    String regEx = "count(\\d+)(df)";
+    String s = "count000dfdfsdffaaaa1";
+    Pattern pat = Pattern.compile(regEx);
+    Matcher mat = pat.matcher(s);
+    if(mat.find()){
+      System.out.println(mat.group(1));
+    }
+  }
+
+  public static void main(String[] args) {
+
+    test1();
+//    test2();
+  }
+}
